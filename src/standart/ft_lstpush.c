74eb719c6d4b41f180f76136124f15e98a17188f
Hoth Mathew <hmathew@il-i3.21-school.ru>
2020-03-03 20:08:07 +0300

    some list func

--- src/standart/ft_lstpush.c
+++ src/standart/ft_lstpush.c
@@ -1,19 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstadd.c                                        :+:      :+:    :+:   */
+/*   ft_lstpush.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hmathew <hmathew@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/09/11 18:35:36 by hmathew           #+#    #+#             */
-/*   Updated: 2020/03/03 20:08:05 by hmathew          ###   ########.fr       */
+/*   Created: 2020/03/03 18:23:14 by hmathew           #+#    #+#             */
+/*   Updated: 2020/03/03 18:37:17 by hmathew          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_lstadd(t_list **alst, t_list *new)
+void	ft_lstpush(t_list **list, t_list *new)
 {
-	new->next = *alst;
-	*alst = new;
+	t_list	*current;
+
+	if (list)
+	{
+		if (*list)
+		{
+			current = *list;
+			while (current->next)
+				current = current->next;
+			current->next = new;
+		}
+		else
+			*list = new;
+	}
 }
--- src/standart/ft_lstrev.c
+++ src/standart/ft_lstrev.c
@@ -1,19 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstadd.c                                        :+:      :+:    :+:   */
+/*   ft_lstrev.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hmathew <hmathew@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/09/11 18:35:36 by hmathew           #+#    #+#             */
-/*   Updated: 2020/03/03 20:08:05 by hmathew          ###   ########.fr       */
+/*   Created: 2020/03/03 18:41:04 by hmathew           #+#    #+#             */
+/*   Updated: 2020/03/03 18:41:26 by hmathew          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_lstadd(t_list **alst, t_list *new)
+void		ft_lstrev(t_list **list)
 {
-	new->next = *alst;
-	*alst = new;
+	t_list	*prev;
+	t_list	*cur;
+	t_list	*next;
+
+	prev = NULL;
+	cur = *list;
+	while (cur != NULL)
+	{
+		next = cur->next;
+		cur->next = prev;
+		prev = cur;
+		cur = next;
+	}
+	*list = prev;
 }
